2d400bdbffb2662a24de7d0ade818a929effb582
Al-Moatasem <accmmsherif@gmail.com>
2022-01-01 00:15:11 +0200

    Applying VS Code formatting

--- create_db_objects.py
+++ create_db_objects.py
@@ -1,13 +1,13 @@
 import os
 import json
 
-with open('config.json', 'r') as config_file:
+with open("config.json", "r") as config_file:
     config = json.load(config_file)
-    src_connection_info = config['database']['sqlserver_stg']
-    src_server = src_connection_info['server']
+    src_connection_info = config["database"]["sqlserver_stg"]
+    src_server = src_connection_info["server"]
 
-    trg_connection_info = config['database']['sqlserver_dw']
-    trg_server = trg_connection_info['server']
+    trg_connection_info = config["database"]["sqlserver_dw"]
+    trg_server = trg_connection_info["server"]
 
 # sqlcmd doc https://docs.microsoft.com/en-us/sql/tools/sqlcmd-utility?redirectedfrom=MSDN&view=sql-server-ver15
 
@@ -15,8 +15,14 @@ with open('config.json', 'r') as config_file:
 # os.system(fr'create_db_objects_sql_server.bat')
 
 # sqlcmd -S {src_server} -U {src_username} -P {src_password} -i sql_file.sql
-os.system(rf'sqlcmd -S {src_server} -E -i sql_server_scripts\101_create_db_and_schemas.sql')
-os.system(rf'sqlcmd -S {src_server} -E -i sql_server_scripts\102_create_stg_utilities.sql')
-os.system(rf'sqlcmd -S {src_server} -E -i sql_server_scripts\201_create_stg_tables.sql')
-os.system(rf'sqlcmd -S {src_server} -E -i sql_server_scripts\301_create_views_stg_to_dwh.sql')
-os.system(rf'sqlcmd -S {trg_server} -E -i sql_server_scripts\401_create_dwh_tables.sql')
+os.system(
+    rf"sqlcmd -S {src_server} -E -i sql_server_scripts\101_create_db_and_schemas.sql"
+)
+os.system(
+    rf"sqlcmd -S {src_server} -E -i sql_server_scripts\102_create_stg_utilities.sql"
+)
+os.system(rf"sqlcmd -S {src_server} -E -i sql_server_scripts\201_create_stg_tables.sql")
+os.system(
+    rf"sqlcmd -S {src_server} -E -i sql_server_scripts\301_create_views_stg_to_dwh.sql"
+)
+os.system(rf"sqlcmd -S {trg_server} -E -i sql_server_scripts\401_create_dwh_tables.sql")
--- stg_read_file_load_table.py
+++ stg_read_file_load_table.py
@@ -1,20 +1,35 @@
-from util import list_directory_files, read_csv_pd, connect_to_sqlserver_db_sqlalchemy, sql_truncate_table, sql_insert_into
+from util import (
+    list_directory_files,
+    read_csv_pd,
+    connect_to_sqlserver_db_sqlalchemy,
+    sql_truncate_table,
+    sql_insert_into,
+)
 from etl_audit import insert_audit_record, Update_audit_record, count_table_records
 import json
 
 
-def read_file_load_db_table(path, schema_target, table_name, connection_info_target, connection_info_etl, mask = '*'):
+def read_file_load_db_table(
+    path,
+    schema_target,
+    table_name,
+    connection_info_target,
+    connection_info_etl,
+    mask="*",
+):
     cnxn_target = connect_to_sqlserver_db_sqlalchemy(connection_info_target)
     cnxn_etl = connect_to_sqlserver_db_sqlalchemy(connection_info_etl)
 
-    audit_key = insert_audit_record(cnxn_etl, -1, f'stg loading {table_name}', path, table_name)
+    audit_key = insert_audit_record(
+        cnxn_etl, -1, f"stg loading {table_name}", path, table_name
+    )
 
     initial_row_count = count_table_records(cnxn_target, schema_target, table_name)
 
-    print(f'Truncating {table_name}')
+    print(f"Truncating {table_name}")
     # Truncating the destination table
     sql_truncate_table(schema_target, table_name, cnxn_target)
-    
+
     rows_extracted = 0
     rows_inserted = 0
     rows_rejected = 0
@@ -24,7 +39,7 @@ def read_file_load_db_table(path, schema_target, table_name, connection_info_tar
         rows_extracted += df.shape[0]
 
         # Inserting data into destination table
-        print(f'Inserting into {table_name}, {file}')
+        print(f"Inserting into {table_name}, {file}")
         try:
             sql_insert_into(df, schema_target, table_name, cnxn_target)
             rows_inserted += df.shape[0]
@@ -34,24 +49,27 @@ def read_file_load_db_table(path, schema_target, table_name, connection_info_tar
     final_row_count = count_table_records(cnxn_target, schema_target, table_name)
 
     Update_audit_record(
-    cnxn_etl, audit_key,
-    initial_row_count = initial_row_count,
-    rows_extracted = rows_extracted,
-    rows_inserted = rows_inserted, 
-    # rows_updated = 0, 
-    rows_rejected = rows_rejected, 
-    final_row_count = final_row_count
+        cnxn_etl,
+        audit_key,
+        initial_row_count=initial_row_count,
+        rows_extracted=rows_extracted,
+        rows_inserted=rows_inserted,
+        # rows_updated = 0,
+        rows_rejected=rows_rejected,
+        final_row_count=final_row_count,
     )
 
 
-if __name__ == '__main__':
-    
-    with open('config.json', 'r') as config_file:
+if __name__ == "__main__":
+
+    with open("config.json", "r") as config_file:
         config = json.load(config_file)
-        connection_info_target = config['database']['sqlserver_stg']
-        connection_info_etl = config['database']['sqlserver_etl']
+        connection_info_target = config["database"]["sqlserver_stg"]
+        connection_info_etl = config["database"]["sqlserver_etl"]
 
-    course_lectures = r'input_files\Course Outlines CSV'
-    table_name = 'CourseLectures'
+    course_lectures = r"input_files\Course Outlines CSV"
+    table_name = "CourseLectures"
 
-    read_file_load_db_table(course_lectures, 'stg', table_name, connection_info_target, connection_info_etl)
+    read_file_load_db_table(
+        course_lectures, "stg", table_name, connection_info_target, connection_info_etl
+    )
