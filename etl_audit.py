eccca8dba40c0ff8d85da87214578e574d280f01
Al-Moatasem <accmmsherif@gmail.com>
2022-01-01 00:29:01 +0200

    Update etl_audit.py
    Replacing the hard coded queries with Stored Procedures

--- etl_audit.py
+++ etl_audit.py
@@ -1,56 +1,60 @@
-from datetime import datetime
-
-
 def insert_audit_record(
     connection, parent_audit_key, etl_job_name, src_file_name, table_name
 ):
 
-    cnxn = connection.raw_connection()
-    crsr = cnxn.cursor()
-
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    crsr.execute(
-        f"""INSERT INTO etl.Audit ( ParentAuditKey, ETLJobName, FileName, TableName, StartDate )
-				OUTPUT INSERTED.AuditKey
-				VALUES
-				( {parent_audit_key}, N'{etl_job_name}', N'{src_file_name}', N'{table_name}', '{now}' )"""
-    )
-
-    AuditKey = crsr.fetchone()[0]
-
-    cnxn.commit()
-    crsr.close()
-    cnxn.close()
-
+    cnxn = connection.connect()
+    transaction = cnxn.begin()
+    AuditKey = cnxn.execute(
+        f"""
+            DECLARE	@AuditKey int;
+            EXEC	etl.InsertAuditRecord 
+                @ParentAuditKey = ?, 
+                @ETLJobName = ?,
+                @FileName = ?, 
+                @TableName = ?, 
+                @AuditKey = @AuditKey OUTPUT
+            """,
+        (parent_audit_key, etl_job_name, src_file_name, table_name),
+    ).fetchone()[0]
+    transaction.commit()
     return AuditKey
 
 
 def Update_audit_record(
     connection,
     audit_key,
-    initial_row_count="NULL",
-    rows_extracted="NULL",
-    rows_inserted="NULL",
-    rows_updated="NULL",
-    rows_rejected="NULL",
-    final_row_count="NULL",
+    initial_row_count=None,
+    rows_extracted=None,
+    rows_inserted=None,
+    rows_updated=None,
+    rows_rejected=None,
+    final_row_count=None,
 ):
 
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    connection.execute(
-        f"""UPDATE etl.Audit
-                SET 
-                    InitialRowCount	= {initial_row_count}, 
-                    RowsExtracted = {rows_extracted},
-                    RowsInserted = {rows_inserted}, 
-                    RowsUpdated = {rows_updated}, 
-                    RowsRejected = {rows_rejected}, 
-                    FinalRowCount = {final_row_count}, 
-                    EndDate = '{now}', 
-                    CompletedSuccessfully = 'Y'
-                WHERE AuditKey = {audit_key}
-            """
+    cnxn = connection.connect()
+    transaction = cnxn.begin()
+    cnxn.execute(
+        f"""
+        EXEC	[etl].[UpdateAuditRecord] 
+            @AuditKey = ?, 
+            @InitialRowCount = ?, 
+            @RowsExtracted = ?,
+		    @RowsInserted = ?, 
+            @RowsUpdated = ?, 
+            @RowsRejected = ?, 
+            @FinalRowCount = ?
+        """,
+        (
+            audit_key,
+            initial_row_count,
+            rows_extracted,
+            rows_inserted,
+            rows_updated,
+            rows_rejected,
+            final_row_count,
+        ),
     )
+    transaction.commit()
 
 
 def count_table_records(connection, schema_name, table_name):
