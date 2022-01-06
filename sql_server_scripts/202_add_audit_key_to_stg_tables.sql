92aff23d2e6bc2ce19026fb8c5906e1d0b0b92af
Al-Moatasem <accmmsherif@gmail.com>
2022-01-07 00:19:51 +0200

    Update 202_add_audit_key_to_stg_tables.sql

--- sql_server_scripts/202_add_audit_key_to_stg_tables.sql
+++ sql_server_scripts/202_add_audit_key_to_stg_tables.sql
@@ -15,6 +15,6 @@ ALTER TABLE stg.CourseInstructors ADD AuditKey INT NOT NULL;
 
 IF NOT EXISTS (
 	select name from sys.columns 
-	where object_id = object_id(N'stg.CourseLectures' ) and name = N'AuditKey'
+	where object_id = object_id(N'stg.CourseBasicInfo' ) and name = N'AuditKey'
 	)
-ALTER TABLE stg.CourseLectures ADD AuditKey INT NOT NULL;
+ALTER TABLE stg.CourseBasicInfo ADD AuditKey INT NOT NULL;
