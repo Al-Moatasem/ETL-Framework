2bd56c488f1fafeb10051f8e9354207bc66a5493
Al-Moatasem <accmmsherif@gmail.com>
2022-01-08 21:46:39 +0200

    archiving processed files

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from stg_read_file_load_table import loop_dir_files_load_db_table
+from stg_read_file_load_table import (
+    read_file_load_db_table,
+)
 from dwh_truncate_load_table import dwh_truncate_and_load
 from etl_audit import insert_audit_record, Update_audit_record
-from util import read_csv_pd, connect_to_sqlserver_db_sqlalchemy
+from util import (
+    archive_processed_file,
+    read_csv_pd,
+    connect_to_sqlserver_db_sqlalchemy,
+    list_directory_files,
+)
 
 
 def execute_staging_master(etl_metadata, connection_info_etl, parent_audit_key):
@@ -21,15 +28,26 @@ def execute_staging_master(etl_metadata, connection_info_etl, parent_audit_key):
         schema_target, table_name = row.table_name_target.split(".")
         files_mask = row.filter_files
 
-        loop_dir_files_load_db_table(
-            path,
-            schema_target,
-            table_name,
-            connection_info_staging,
-            connection_info_etl,
-            parent_audit_key=audit_key,
-            files_mask=files_mask,
-        )
+        for idx, file in enumerate(list_directory_files(path, files_mask)):
+            truncate_trg_table = True
+
+            # Truncate target table with the first iteration only.
+            if idx:
+                truncate_trg_table = False
+
+            read_file_load_db_table(
+                file,
+                schema_target,
+                table_name,
+                connection_info_staging,
+                connection_info_etl,
+                parent_audit_key,
+                truncate_trg_table,
+            )
+
+            # Archiving processed files
+            archiving_path = row.archive_path
+            archive_processed_file(file, archiving_path, move_file=True)
 
     Update_audit_record(
         cnxn_etl,
